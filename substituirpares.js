5581a15c620821310a72625828dc698aa2d6abaf
RaffaelMiguell <raffaelmiguell@gmail.com>
2022-07-05 02:31:05 -0300

    algoritimo para substituir números pares de um array

--- substituirpares.js
+++ substituirpares.js
@@ -8,6 +8,23 @@ function substituiPares(array){
   for(let i=0; i<array.length; i++){
     if(array[i]===0){
     console.log("O númeor informado já é zero.")
-    }else if(array[i]%2===0)
+    }else if(array[i]%2===0){
+      console.log(`Substituindo ${array[i]} por 0...`);
+      array[i]=0;
+    }
   }
+  return array;
 }
+
+let array=[1,2,3,4,5,6,7,8,9,10];
+
+substituiPares(array);
+
+// console.log(array)
+
+
+
+
+// for para percorrer o array
+// if para verificar se índice do array é 0
+// else if para verificar o resto da divisão por 2 (par) - se sim, troca o índice atual por 0
